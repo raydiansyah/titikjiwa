6249088df5ce19ff841f5a31e701f7d03f6fef52
Ray Diansyah <raydiansyah@gmail.com>
2026-08-23 22:54:47 +0700

    feat: load responsive workspace navigation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import { initializeTheme } from "@/lib/theme";
 import "@/index.css";
 import App from "@/App";
 import "@/theme-responsive.css";
+import "@/workspace-responsive.css";
 
 // Apply the persisted/system theme before React paints to avoid a light/dark flash.
 initializeTheme();
