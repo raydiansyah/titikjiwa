ccbfc470923d54494f0d7f7cf725f0323c59a96f
Ray Diansyah <raydiansyah@gmail.com>
2026-08-23 22:41:22 +0700

    feat: load responsive light and dark theme styles

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@/index.css";
 import App from "@/App";
+import "@/theme-responsive.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
